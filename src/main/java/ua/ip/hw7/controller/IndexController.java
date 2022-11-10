147ff4e705f60e34dea86c025dc08d3b98571bfb
GrafMK <grafmk1523@gmail.com>
2022-11-10 18:36:01 +0200

    finishCommitWithFlyWay

--- src/main/java/ua/ip/hw7/controller/IndexController.java
+++ src/main/java/ua/ip/hw7/controller/IndexController.java
@@ -1,5 +1,7 @@
 package ua.ip.hw7.controller;
 
+import ua.ip.hw7.storage.DatabaseInitService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,12 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/")
 public class IndexController extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        new DatabaseInitService().initDb("jdbc:postgresql://localhost:5432/GrafProductCompany"
+                ,"YourUsername"
+                ,"YourPassword");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/ua/ip/hw7/storage/DatabaseInitService.java
+++ src/main/java/ua/ip/hw7/storage/DatabaseInitService.java
@@ -1,13 +1,13 @@
-//package ua.ip.hw7.storage;
-//
-//import org.flywaydb.core.Flyway;
-//
-//public class DatabaseInitService {
-//    public void initDb(String Url, String user,String pass){
-//        Flyway flyway = Flyway
-//                .configure()
-//                .dataSource(Url,user,pass)
-//                .load();
-//        flyway.migrate();
-//    }
-//}
+package ua.ip.hw7.storage;
+
+import org.flywaydb.core.Flyway;
+
+public class DatabaseInitService {
+    public void initDb(String Url, String user,String pass){
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(Url,user,pass)
+                .load();
+        flyway.migrate();
+    }
+}
